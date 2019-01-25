74e2ed1ce228c684b2e82178947508e24b6cd46c
Jeremy Arrieta <jeremyretamararrieta@gmail.com>
2019-01-24 17:01:38 -0700

    commit message

---
+++ exercises/hello-class/app.js
@@ -0,0 +1,14 @@
+/* let name = "Jere"
+let age = 26
+
+let Hello = function(){
+    console.log("hello " + name + " do you like being "+ age + "?")
+}
+
+Hello() */
+
+
+function sayHello(age , name){
+    console.log("hi " + name + " do you like being " + age + "?");
+}
+sayHello(23, "jane")
