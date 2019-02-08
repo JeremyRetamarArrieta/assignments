9b0d6eef060ed97f69a76f05543500cc0e23d6b3
Jeremy Arrieta <jeremyretamararrieta@gmail.com>
2019-02-07 17:05:53 -0700

    feedback added

---
+++ exercises/filter-excersies/app.js
@@ -0,0 +1,75 @@
+// Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
+
+
+// function fiveAndGreaterOnly(arr) {
+// //code here
+//     return arr.filter(function(num){
+//         if (num > 5){
+//             return num 
+//         }
+        
+//     })  
+// }
+// //test
+
+// console.log(fiveAndGreaterOnly([3,6,8,2]));
+
+// // let adults = family.filter(person => person.age > 18)
+
+// // Given an array of numbers, return a new array that only includes the even numbers.
+
+// function evensOnly(arr){
+//     return arr.filter(function(even){
+//         if ( even % 2 === 0){
+//         return even
+//         }
+//     })
+// }
+
+// console.log(evensOnly([3,6,8,2]));
+
+//3) Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length
+
+// function fiveCharactersOrFewerOnly(arr){
+//     return arr.filter(function(fiveOrless){
+//         if( fiveOrless <= 5){ 
+//         }return fiveOrless
+//     })
+// }
+
+// console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"])); // ["by", "dog", "wolf", "eaten"]
+
+
+//Given an array of people objects, return a new array that has filtered out all those who don't belong to the club.
+
+function memberOfIluminati(arr){
+    // your code here
+return arr.filter(function(member){
+    if ( === true){
+        }return member
+    })
+  }
+  // test
+  console.log(memberOfIluminati([
+      {
+          name: "Angelina Jolie",
+          member: true
+      },
+      {
+          name: "Eric Jones",
+          member: false
+      },
+      {
+          name: "Paris Hilton",
+          member: true
+      },
+      {
+          name: "Kayne West",
+          member: false
+      },
+      {
+          name: "Bob Ziroll",
+          member: true
+      }
+  ]));
+  
--- exercises/notes-practice/calling-methods-in-functions/app.js
+++ exercises/notes-practice/calling-methods-in-functions/app.js
@@ -1,14 +1,40 @@
-function compute(num1, num2, callback){
-    return callback(num1,num2)
-}
+// function compute(num1, num2, callback){
+//     return callback(num1,num2)
+// }
 
-function add(num1,num2){
-    return num1 + num2
-}
+// function add(num1,num2){
+//     return num1 + num2
+// }
 
-function subtract(num1,num2){
-    return num1 - num2
-}
+// function subtract(num1,num2){
+//     return num1 - num2
+// }
 
-console.log(compute(3,5, add))
-console.log(compute(67,5, subtract))
+// console.log(compute(3,5, add))
+// console.log(compute(67,5, subtract))
+
+
+
+
+
+// function peopleOver50(arr){
+//     var over50 = arr.filter(function(user){
+//         if (user.age> 50){
+//             return user
+//         }
+//     })
+//     return over50
+// }
+
+// .map() => returns a array containing  an item every in the orignial array
+
+users[{
+    name
+}]
+
+
+let usersnames = users.map(function(user){
+    return user.name
+})
+
+console.log(usernames)
