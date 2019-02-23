f3ee6e86fa5714f3515b75e972c87eb1c9de753a
Jeremy Arrieta <jeremyretamararrieta@gmail.com>
2019-02-22 17:13:03 -0700

    excersises added

---
+++ exercises/superheros-react/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+class App extends React.Component {
+    render(){
+        const mappedSuperheroes = data.superHeros.map((hero, i))
+    }
+}
+    
---
+++ exercises/superheros-react/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import data from './superHeroes.json'
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
